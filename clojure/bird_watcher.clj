cb87b2ce3ba8c3b60dc1fd906416055ed3837241
f.areias <nando.calheirosx@gmail.com>
2024-10-20 09:36:52 -0300

    Add clojure

---
+++ clojure/bird_watcher.clj
@@ -0,0 +1,23 @@
+(ns bird-watcher)
+
+(def last-week 
+  [0 2 5 3 7 8 4])
+
+(defn today [birds]
+  (last birds))
+
+(defn inc-bird [birds]
+  (conj (vec (butlast birds)) (inc (last birds))))
+
+(defn day-without-birds? [birds]
+  (boolean (some zero? birds))) 
+
+(defn n-days-count [birds n]
+  (reduce + (take n birds)))
+
+(defn busy-days [birds]
+  (count (filter #(>= % 5) birds)))
+
+(defn odd-week? [birds]
+  (let [expected-pattern (cycle [1 0])]  
+    (= birds (take (count birds) expected-pattern))))
